d256f6dcebd6a5ee715b990b4060afeee41c85aa
Felipe Morales Simões <100170616+FelipeMoralesdev@users.noreply.github.com>
2023-04-29 16:18:27 -0300

    Seguindo ex do curso Curso React Native OBC aula 7 part 2

--- App.js
+++ App.js
@@ -1,36 +1,19 @@
-import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Contatos from './src/pages/Contatos';
+import Informacoes from './src/pages/Informacoes';
 
 const Drawer = createDrawerNavigator();
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      <Drawer.Navigator>
+        <Drawer.Screen name="Contatos" component={Contatos}/>
+        <Drawer.Screen name="Informações" component={Informacoes}/>
       </Drawer.Navigator>
     </NavigationContainer>
-  );
+  )
 }
